9211809bbc1e6f6057cd416fb4b834488dc0fcf3
jithendra <mittajithendra46@gmail.com>
2021-04-06 18:00:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const routes= require('./routes/web');
 const bodyParser = require('body-parser');
 const app = express();
+var allowedOrigins = [origionDomain] 
+res.header('Access-Control-Allow-Origin', allowedOrigins)
 
 
 app.set('views', './views');
